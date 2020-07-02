8932713d4cee800e86baa5abc6f68978cc18b239
yalenachothia <yalena.chothia@gmail.com>
2020-07-02 11:46:00 +0100

    Programming Assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+#--------------------------------
+# Yalena H. Chothia, 02.07.2020
+# Programming Assignment 2: write a pair of functions that cache the inverse of a matrix.
+# makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+# cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+#--------------------------------
 
-## Write a short comment describing this function
+# 'makeCacheMatrix' is a function that makes a cache matrix from a given matrix
+# The cache matrix is initialised by assigning the value 'NULL'
+# 'setMatrix' assigns a value to x, giving a matrix
+# 'getMatrix' returns the matrix assigned to x
+# 'setCache' inverts the cached matrix
+# 'getCache' returns the cached matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cacheMatrix <- NULL
+  setMatrix <- function(y) {
+    x <<- y
+    cacheMatrix = NULL
+  }
+  getMatrix <- function() x
+  setCache <- function(inverse) cacheMatrix <<- inverse
+  getCache <- function() cacheMatrix
+  list(setMatrix = setMatrix,
+       getMatrix = getMatrix,
+       setCache = setCache,
+       getCache = getCache)
 }
 
-
-## Write a short comment describing this function
-
+# 'cacheSolve' is a function that returns the inverse of a given matrix using the cache
+# This is dependent on the contents of the cache.
+# If the contents is not 'NULL' then the result is returned 
+# If the contents is empty then the cache is created and set.
+# The cache is updated and the cache matrix is returned
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cacheMatrix <- x$getCache()
+  if (!is.null(cacheMatrix)) {
+    message("loading cache matrix...")
+    return(cacheMatrix)
+  }
+  else {
+    dMatrix <- x$getMatrix()
+    cacheMatrix <- solve(dMatrix, ...)
+    x$setCache(cacheMatrix)
+    return(cacheMatrix)
+  }
 }
